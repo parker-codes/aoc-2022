6ce4639c32e737cbe2eff8ad2c4cb7c4756d224e
Parker McMullin <parker.mcmullin01@gmail.com>
2022-12-01 16:04:56 -0700

    Solve part one of day one.

---
+++ src/bin/01.rs
@@ -0,0 +1,37 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    input
+        .split_terminator("\n\n")
+        .map(|list| {
+            list.lines()
+                .filter_map(|s| -> Option<u32> { s.parse().ok() })
+                .sum()
+        })
+        .max()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), Some(24_000));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
